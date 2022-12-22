6eb084e4f78e33755204b22782e0315946cf22f8
yuxinlin <yuxinlin93@gmail.com>
2022-12-22 10:35:47 +0800

    p00-1

--- exam/p/p00.py
+++ exam/p/p00.py
@@ -1,6 +1,10 @@
 def p00(n=0):
     even=None
     # ↓程式區域↓
-
+    input = input("input int")
+    if(input%2==0):
+        even="Ture"
+    else:
+        even='False'
     # ↑程式區域↑
     return even
